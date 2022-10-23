e7609c5c22d0b9c6a081f0bb1b88f66548635aa0
balay <dlolhd@gmail.com>
2022-10-23 12:01:37 +0200

    Creación de composable GameLayout y añadidos recursos de strings.

--- app/src/main/java/com/diego/kotlin/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/diego/kotlin/unscramble/ui/GameScreen.kt
@@ -1,9 +1,62 @@
 package com.diego.kotlin.unscramble.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.diego.kotlin.unscramble.R
 
 @Composable
 fun GameScreen() {
 
+}
+
+@Composable
+fun GameLayout(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        Text(
+            text = "scrambleun",
+            fontSize = 45.sp,
+            modifier = modifier.align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = stringResource(R.string.instructions),
+            fontSize = 17.sp,
+            modifier = modifier.align(Alignment.CenterHorizontally)
+        )
+        OutlinedTextField(
+            value = "",
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { },
+            label = { Text(stringResource(R.string.enter_your_word)) },
+            isError = false,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {  }
+            ),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GameLayoutPreview() {
+    GameLayout()
 }
 
